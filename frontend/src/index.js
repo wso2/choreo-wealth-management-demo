62be41480fe7b799cdbb91ac5d7dbec5652a8a82
Aruni Wijesuriya <aruniw88@gmail.com>
2023-01-05 14:03:26 +0530

    Login page layout and improvements

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './App.scss';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 
